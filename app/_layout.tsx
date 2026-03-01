e51b2c578e8af07624bb86764af270a87fa9d39a
Yanal <yanal.sweseh@gmail.com>
2026-03-01 13:08:40 +0200

    Add new code for products service

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -26,7 +26,7 @@ export default function TabLayout() {
         <Tabs.Screen
             name="login"
             options={{
-                title: 'login',
+                title: 'Login',
                 tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
             }}
         />
