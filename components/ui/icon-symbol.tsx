2bebf66d366307332e15667d0cd2bdce239c1e13
Yanal <yanal.sweseh@gmail.com>
2026-04-23 01:52:35 +0300

    Add new code for Gesture

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -16,6 +16,7 @@ type IconSymbolName = keyof typeof MAPPING;
 const MAPPING = {
   'house.fill': 'home',
   'paperplane.fill': 'send',
+  'externaldrive.fill': 'storage',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
 } as IconMapping;
