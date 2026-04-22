938980b66d76fc2e206a34988dad85a2875f0329
Yanal <yanal.sweseh@gmail.com>
2026-04-23 01:39:14 +0300

    Add new code for Gesture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,23 @@
-import {StyleSheet, Text, View} from "react-native";
+import {Pressable, StyleSheet, Text, View} from "react-native";
 import A from "@/components/A";
 import B from "@/components/B";
 import {useEffect, useState} from "react";
 import StorageService from "@/services/StorageService";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 function HomeScreen () {
     const [user, setUser] = useState<any>("");
 
+
+
+    const tap = Gesture.Tap().onEnd(() => {
+        console.log("tapped");
+    });
+
+    const pan = Gesture.Pan().onEnd((e) => {
+        console.log("update", e);
+
+    })
     const logout = async () => {
         StorageService.removeToken();
         StorageService.clearAsyncStorage();
@@ -26,9 +37,10 @@ function HomeScreen () {
 
     return (
         <View style={styles.container}>
-            <Text>{user.firstName}</Text>
-            <A></A>
-            <B></B>
+            <GestureDetector gesture={pan}>
+                <Text>text 1111</Text>
+            </GestureDetector>
+            <Pressable onPress={logout} style={({ pressed }) => ({ opacity: pressed ? 0.5 : 1 })}></Pressable>
         </View>
     )
 }
